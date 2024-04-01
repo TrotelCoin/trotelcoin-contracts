a317ac582a0ca26828928341a8c0c9d332da4cb5
Alexandre Trotel <trotelalexandre@proton.me>
2024-04-01 16:12:50 +0200

    fix: trotelcoin v2

--- TrotelCoinV2.sol
+++ TrotelCoinV2.sol
@@ -8,11 +8,11 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/
 import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/release-v4.9/contracts/proxy/utils/Initializable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/release-v4.9/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/release-v4.9/contracts/proxy/utils/UUPSUpgradeable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/release-v4.9/contracts/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 
-contract TrotelCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable, ERC20CappedUpgradeable {
+contract TrotelCoinV2 is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    uint256 private _cap;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -27,7 +27,7 @@ contract TrotelCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
         __AccessControl_init();
         __ERC20Permit_init("TrotelCoin");
         __UUPSUpgradeable_init();
-        __ERC20Capped_init(1000000000 * 10 ** decimals());
+        _cap = 1000000000 * 10 ** decimals();
 
         _mint(msg.sender, 100000000 * 10 ** decimals());
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
@@ -36,11 +36,16 @@ contract TrotelCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= _cap, "Cap exceeded");
         _mint(to, amount);
     }
 
-    function _mint(address account, uint256 amount) internal override(ERC20Upgradeable, ERC20CappedUpgradeable) {
-        ERC20CappedUpgradeable._mint(account, amount);
+    function cap() public view returns(uint256) {
+        return _cap;
+    }
+
+    function setCap(uint256 _newCap) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _cap = _newCap;
     }
 
     function _authorizeUpgrade(address newImplementation)
